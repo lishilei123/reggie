92cd679f0a894c1d50f212cd7fa4df1c53690112
LiShilei <972971617@qq.com>
2024-07-26 21:42:15 +0800

    提交到分支1上的内容

---
+++ src/main/java/com/itheima/reggie/mapper/EmployeeMapper.java
@@ -0,0 +1,15 @@
+package com.itheima.reggie.mapper;
+
+
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.itheima.reggie.entity.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.stream.BaseStream;
+
+
+@Mapper
+public interface EmployeeMapper extends BaseMapper <Employee>{
+}
